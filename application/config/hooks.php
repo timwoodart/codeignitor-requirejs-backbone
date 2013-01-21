b033cca63da936736c601cb179c439e39588938a
Tim Wood <timwoodwood@gmail.com>
2013-01-20 22:46:51 -0500

    Enable Hooks to load custom layouts, see http://www.syahzul.com/blogs/item/codeigniter-layout-without-using-additional-library

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -10,6 +10,16 @@
 |
 */
 
+/*
+ * Overrides the default display layout
+ *
+*/
+$hook['display_override'] = array(
+    'class'    => 'Layout',
+    'function' => 'index',
+    'filename' => 'Layout.php',
+    'filepath' => 'hooks'
+);
 
 
 /* End of file hooks.php */
