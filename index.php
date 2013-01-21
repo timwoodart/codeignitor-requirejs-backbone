98ce6dc6c0855340b2f8758fab589391b884d130
Tim Wood <timwoodwood@gmail.com>
2013-01-20 22:34:28 -0500

    Setup local environment. This is just a preference of mine to display errors in window

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
-	define('ENVIRONMENT', 'development');
+	define('ENVIRONMENT', 'local');
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
@@ -35,7 +35,10 @@ if (defined('ENVIRONMENT'))
 		case 'development':
 			error_reporting(E_ALL);
 		break;
-	
+		case 'local':
+			error_reporting(E_ALL);
+			ini_set('display_errors',1);
+		break;	
 		case 'testing':
 		case 'production':
 			error_reporting(0);
